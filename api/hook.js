c062d5d089de6dd56507634e7808720d43bd1613
Lawrence Li <lonor@live.com>
2021-01-21 17:03:40 +0800

    add: issue lable demo

--- api/hook.js
+++ api/hook.js
@@ -1,18 +1,31 @@
 const createHandler = require('github-webhook-handler')
 const handler = createHandler({ path: '/api/hook', secret: 'secret' })
+const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({
+    auth: process.env.BOT_TOKEN,
+});
 
 handler.on('issue_comment', function (event) {
     const {
-        repository: { name: repoName },
+        repository: { name: repoName, owner: { login: ownerName } },
         action,
         issue: { number: issueNumber, title: issueTitle },
         comment: { body: commentBody }
     } = event.payload
-    
-    console.log(`Received an issue comment event for ${repoName} action: ${action}'\n
+
+    console.log(`Received an issue comment event for ${ownerName}/${repoName} action: ${action}\n
         issue: #${issueNumber}\n
         title: ${issueTitle}\n
         comment: ${commentBody}`)
+
+    console.log("Call GitHub REST API to send data...")
+    octokit.issues.addLabels({
+        owner: ownerName,
+        repo: repoName,
+        issue_number: issueNumber,
+        labels: commentBody,
+    });
 })
 
 module.exports = (req, res) => {
