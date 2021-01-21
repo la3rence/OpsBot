140003439a7566b5874935b9d7169d28f479a506
Lawrence Li <lonor@live.com>
2021-01-21 16:39:30 +0800

    fix: handler callback

--- api/hook.js
+++ api/hook.js
@@ -1,24 +1,23 @@
 const createHandler = require('github-webhook-handler')
 const handler = createHandler({ path: '/api/hook', secret: 'secret' })
 
+handler.on('issue_comment', function (event) {
+    const {
+        repository: { name: repoName },
+        action,
+        issue: { number: issueNumber, title: issueTitle },
+        comment: { body: commentBody }
+    } = event.payload
+    
+    console.log(`Received an issue comment event for ${repoName} action: ${action}'\n
+        issue: #${issueNumber}\n
+        title: ${issueTitle}\n
+        comment: ${commentBody}`)
+})
+
 module.exports = (req, res) => {
     handler(req, res, function (err) {
         console.log(err)
     })
-
-    handler.on('issue_comment', function (event) {
-        const {
-            repository: { name: repoName },
-            action,
-            issue: { number: issueNumber, title: issueTitle },
-            comment: { body: commentBody }
-        } = event.payload
-        
-        console.log(`Received an issue comment event for ${repoName} action: ${action}'\n
-            issue: #${issueNumber}\n
-            title: ${issueTitle}\n
-            comment: ${commentBody}`)
-    })
-
 }
 
