3eb0a9f4857aa1cf0ea46cb83d25f23dca07c3e9
Lawrence Li <lonor@live.com>
2021-01-22 15:09:06 +0800

    add: github dependency

--- api/index.go
+++ api/index.go
@@ -2,9 +2,46 @@ package handler
 
 import (
 	"fmt"
+	"github.com/google/go-github/github"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	payload, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("error reading request body: err=%s\n", err)
+		return
+	}
+	defer r.Body.Close()
+	event, err := github.ParseWebHook(github.WebHookType(r), payload)
+	if err != nil {
+		log.Printf("could not parse webhook: err=%s\n", err)
+		return
+	}
+	switch e := event.(type) {
+	case *github.PushEvent:
+		// this is a commit push, do something with it
+	case *github.PullRequestEvent:
+		// this is a pull request, do something with it
+	case *github.WatchEvent:
+		// https://developer.github.com/v3/activity/events/types/#watchevent
+		// someone starred our repository
+		if e.Action != nil && *e.Action == "starred" {
+			fmt.Printf("%s starred repository %s\n",
+				*e.Sender.Login, *e.Repo.FullName)
+		}
+	case *github.IssueCommentEvent:
+		fmt.Println("IssueCommentEvent: ")
+		fmt.Println(*e.Action)
+		fmt.Println(*e.GetRepo().Name)
+		fmt.Println(*e.GetIssue().Number)
+		fmt.Println(*e.GetComment().Body)
+		// todo: call GitHub api
+	default:
+		log.Printf("unknown event type %s\n", github.WebHookType(r))
+		return
+	}
+	//fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module OpsBot
 
 go 1.15
+
+require (
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+)
