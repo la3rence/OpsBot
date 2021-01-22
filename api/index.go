b170f58665d71db6aebf626a79dfd8a6e8459524
Lawrence Li <lonor@live.com>
2021-01-22 15:40:49 +0800

    feat: label by comment

--- api/index.go
+++ api/index.go
@@ -1,12 +1,25 @@
 package handler
 
 import (
+	"context"
 	"fmt"
 	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
+
+func getGitHubClient() *github.Client {
+	token := os.Getenv("BOT_TOKEN")
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	return github.NewClient(tc)
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	payload, err := ioutil.ReadAll(r.Body)
@@ -35,10 +48,22 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	case *github.IssueCommentEvent:
 		fmt.Println("IssueCommentEvent: ")
 		fmt.Println(*e.Action)
-		fmt.Println(*e.GetRepo().Name)
-		fmt.Println(*e.GetIssue().Number)
-		fmt.Println(*e.GetComment().Body)
-		// todo: call GitHub api
+		action := *e.Action
+
+		if action == "edited" || action == "created" {
+			client := getGitHubClient()
+			labelName := *e.GetComment().Body
+			labels := []string{labelName}
+			issue, response, err := client.Issues.AddLabelsToIssue(context.Background(), *e.GetRepo().Owner.Login,
+				*e.GetRepo().Name,
+				*e.GetIssue().Number, labels)
+			if err != nil {
+				log.Print(err)
+			}
+			log.Println(response, issue)
+			fmt.Fprintf(w, "ok")
+		}
+
 	default:
 		log.Printf("unknown event type %s\n", github.WebHookType(r))
 		return
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module OpsBot
 go 1.15
 
 require (
+	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/google/go-github v17.0.0+incompatible // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/oauth2 v0.0.0-20210113205817-d3ed898aa8a3 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 )
