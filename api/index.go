72357d39ea565ac4f0f136b8878e6c07acf06f61
Lawrence Li <lonor@live.com>
2021-01-25 18:45:45 +0800

    feat: remove label from issue

--- api/index.go
+++ api/index.go
@@ -14,7 +14,8 @@ import (
 )
 
 const (
-	LABEL = "/label"
+	LABEL   = "/label"
+	UNLABEL = "/un-label"
 )
 
 func getGitHubClient() *github.Client {
@@ -58,32 +59,57 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		commentBody := *e.GetComment().Body
 		action := *e.Action
 		if action == "edited" || action == "created" {
+			issueCommentEvent := *e
 			if strings.Contains(commentBody, LABEL) {
-				// 获取 /label 后的 字符串，注意越界问题
-				wordArray := strings.Fields(commentBody)
-				labelIndex := utils.StringIndexOf(wordArray, LABEL)
-				if len(wordArray) > labelIndex+1 {
-					labelName := wordArray[labelIndex+1]
-					if labelName != "" {
-						labels := []string{labelName}
-						issue, response, err := githubClient.Issues.AddLabelsToIssue(context.Background(), *e.GetRepo().Owner.Login,
-							*e.GetRepo().Name,
-							*e.GetIssue().Number, labels)
-						if err != nil {
-							log.Print(err)
-						}
-						log.Println(response, issue)
-						fmt.Fprintf(w, "ok")
-					}
-				}
-
+				addLabelsToIssue(commentBody, githubClient, issueCommentEvent)
 			}
-
+			if strings.Contains(commentBody, UNLABEL) {
+				removeLabelFromIssue(commentBody, githubClient, issueCommentEvent)
+			}
 		}
+		fmt.Fprintf(w, "ok")
 
 	default:
 		log.Printf("unknown event type %s\n", github.WebHookType(r))
 		return
 	}
 	//fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+}
+
+func addLabelsToIssue(commentBody string, githubClient *github.Client, issueCommentEvent github.IssueCommentEvent) {
+	// 获取 /label 后的 字符串，注意越界问题
+	wordArray := strings.Fields(commentBody)
+	labelIndex := utils.StringIndexOf(wordArray, LABEL)
+	if len(wordArray) > labelIndex+1 {
+		labelName := wordArray[labelIndex+1]
+		if labelName != "" {
+			labels := []string{labelName}
+			issue, response, err := githubClient.Issues.AddLabelsToIssue(context.Background(), *issueCommentEvent.GetRepo().Owner.Login,
+				*issueCommentEvent.GetRepo().Name,
+				*issueCommentEvent.GetIssue().Number, labels)
+			if err != nil {
+				log.Print(err)
+			}
+			log.Println(response, issue)
+		}
+	}
+}
+
+func removeLabelFromIssue(commentBody string, githubClient *github.Client, issueCommentEvent github.IssueCommentEvent) {
+	wordArray := strings.Fields(commentBody)
+	unLabelIndex := utils.StringIndexOf(wordArray, UNLABEL)
+	if len(wordArray) > unLabelIndex+1 {
+		unLabelName := wordArray[unLabelIndex+1]
+		if unLabelName != "" {
+			response, err := githubClient.Issues.RemoveLabelForIssue(context.Background(),
+				*issueCommentEvent.GetRepo().Owner.Login,
+				*issueCommentEvent.GetRepo().Name,
+				*issueCommentEvent.GetIssue().Number,
+				unLabelName)
+			if err != nil {
+				log.Print(err)
+			}
+			log.Println(response)
+		}
+	}
 }
