ae4149b6d89f5c95c71c6beb30143fbe485e338c
Lawrence Li <lonor@live.com>
2021-01-26 16:21:35 +0800

    fix: use sdk getter

--- api/index.go
+++ api/index.go
@@ -138,14 +138,11 @@ func requestReviewIfPROpen(githubClient *github.Client, pullRequestEvent github.
 }
 
 func mergePullRequest(githubClient *github.Client, issueCommentEvent github.IssueCommentEvent) {
-	owner := new(string)
-	repo := new(string)
-	number := new(int)
-	owner = issueCommentEvent.GetRepo().Owner.Name
-	repo = issueCommentEvent.GetRepo().Name
-	number = issueCommentEvent.GetIssue().Number
-	log.Println("147", owner, repo, number)
-	log.Println("148", *owner, *repo, *number)
+	owner := issueCommentEvent.GetRepo().GetOwner().Name
+	repo := issueCommentEvent.GetRepo().Name
+	number := issueCommentEvent.GetIssue().Number
+	log.Println("144", owner, repo, number)
+	log.Println("145", *owner, *repo, *number)
 	mergedBefore, _, _ := githubClient.PullRequests.IsMerged(context.Background(), *owner, *repo, *number)
 	mergeComment := fmt.Sprintf("PR #%d was merged.", number)
 	commitMsg := fmt.Sprintf("merge: PR(#%d)", number)
