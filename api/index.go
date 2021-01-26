bc711482fb164760cd7309d572b09a138d702de4
Lawrence Li <lonor@live.com>
2021-01-26 16:06:00 +0800

    test: *string

--- api/index.go
+++ api/index.go
@@ -144,6 +144,8 @@ func mergePullRequest(githubClient *github.Client, issueCommentEvent github.Issu
 	owner = issueCommentEvent.GetRepo().Owner.Name
 	repo = issueCommentEvent.GetRepo().Name
 	number = issueCommentEvent.GetIssue().Number
+	log.Print(owner, repo, number)
+	log.Printf(*owner, *repo, *number)
 	mergedBefore, _, _ := githubClient.PullRequests.IsMerged(context.Background(), *owner, *repo, *number)
 	mergeComment := fmt.Sprintf("PR #%d was merged.", number)
 	commitMsg := fmt.Sprintf("merge: PR(#%d)", number)
