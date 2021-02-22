8a22f5a4949a23f21d26531e14078ec1850ce4ac
Lawrence Li <lonor@live.com>
2021-02-22 11:53:56 +0800

    test: stringutils

--- utils/stringutils.go
+++ utils/stringutils.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -18,4 +20,24 @@ func StringIndexOf(originalArray []string, wordToFind interface{}) []int {
 		}
 	}
 	return indexArray
+}
+
+func StringToInt32(numberString string) (int32, error) {
+	parseNumber, err := strconv.ParseInt(numberString, 10, 32)
+	if err == nil {
+		return int32(parseNumber), nil
+	} else {
+		return 0, err
+	}
+}
+
+// 封装：获取 /tag 后一位字符串
+func GetTagNextOneParam(originalMessage string, tagName string) (nextString string, err error) {
+	wordArray := strings.Fields(originalMessage)
+	indexes := StringIndexOf(wordArray, tagName)
+	if len(wordArray) > indexes[0]+1 {
+		nextString = wordArray[indexes[0]+1]
+		return nextString, nil
+	}
+	return nextString, fmt.Errorf("param of %s required", tagName)
 }
