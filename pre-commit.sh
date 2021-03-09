644b89d670bf3eba7ee7637498cf4098bc7be766
Lawrence Li <lonor@live.com>
2021-03-09 15:55:22 +0800

    dependency: bump Go version to 1.16

---
+++ pre-commit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ln -s $PWD/pre-commit.sh .git/hooks/pre-commit
+go test ./...
+RESULT=$?
+if [[ $RESULT != 0 ]]; then
+    echo "REJECTING COMMIT (test failed with status: $RESULT)"
+    exit 1;
+fi
+
+go fmt ./...
+exit 0
